571daa3d8f03b9ee00771ffc4e5d3c0b9dc205f7
Nick Peachey <nick.peachey@gmail.com>
2022-10-25 17:56:48 +0100

    added docker compose build command

---
+++ src/Services/Catalog/Catalog.API/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<string> Home()
+        {
+            return await Task.Run(() => "hello world");
+        }
+    }
+}
