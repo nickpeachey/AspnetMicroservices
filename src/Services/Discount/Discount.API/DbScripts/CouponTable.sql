be53667d1896e74ce5508da59d4e857140a0393b
Nick Peachey <nick.peachey@gmail.com>
2022-10-27 14:25:06 +0100

    added postgres and pgadmin

---
+++ src/Services/Discount/Discount.API/DbScripts/CouponTable.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE Coupon (
+		ID SERIAL PRIMARY KEY		NOT NULL,
+		ProductName		VARCHAR(24) NOT NULL,
+		Description		TEXT,
+		Amount			INT
+);
+
+
+INSERT INTO Coupon (ProductName, description, amount) VALUES ('IPhone X', 'IPhone Discount', 150);
+
+INSERT INTO Coupon (ProductName, description, amount) VALUES ('Samsung 10', 'Samsung Discount', 100);
